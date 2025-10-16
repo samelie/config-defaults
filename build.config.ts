5201c9189976ca980f9c0e379b69d05caacde674
Sam Elie <samradelie@gmail.com>
2025-10-16 08:55:12 -0700

    0.0.6

--- build.config.ts
+++ build.config.ts
@@ -1,16 +1,12 @@
 import type { IPackageJson } from "package-json-type";
 import { readFileSync } from "node:fs";
-import { parse } from "node:path";
+import { join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { makeUnbuildConfig } from "@adddog/build-configs/unbuild";
-import { packageUpSync } from "package-up";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
-const p = packageUpSync({
-    cwd: parse(__dirname).dir,
-});
-const pkgJson = JSON.parse(readFileSync(p || "", "utf-8")) as IPackageJson;
+const pkgJson = JSON.parse(readFileSync(join(__dirname, "package.json") || "", "utf-8")) as IPackageJson;
 
 export default makeUnbuildConfig({
     entries: [
@@ -18,5 +14,8 @@ export default makeUnbuildConfig({
         "src/vite.config",
     ],
     declaration: true,
-    externals: [...Object.keys(pkgJson.devDependencies || [])],
+    externals: [
+        ...Object.keys(pkgJson.dependencies || {}),
+        ...Object.keys(pkgJson.devDependencies || {}),
+    ],
 });
